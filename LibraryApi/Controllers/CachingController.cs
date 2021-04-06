bb64c95edc4b26966e5456c525cf3f07f0cb03e1
Damon Achey <damon_j_achey@progressive.com>
2021-04-06 15:42:33 -0400

    Added new controller to play with caching

---
+++ LibraryApi/Controllers/CachingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class CachingController : ControllerBase
+    {
+        [HttpGet("/ServerTime")]
+        [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 10)]
+        public ActionResult GetTheTime()
+        {
+            return Ok(new { ServerTime = DateTime.Now });
+        }
+    }
+}
