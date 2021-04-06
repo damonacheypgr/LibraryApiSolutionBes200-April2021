21a5b1b192f5ae1a0aa5b21b362342541dd00f17
Damon Achey <damon_j_achey@progressive.com>
2021-04-06 16:00:26 -0400

    added test client

---
+++ TimeClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TimeClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:1337");
+
+            while (true)
+            {
+                var response = await client.GetAsync("/servertime");
+
+                Console.WriteLine(response.Headers.CacheControl.ToString());
+
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+
+                Console.WriteLine("Continue");
+                var q = Console.ReadLine();
+
+                if (q == "q") break;
+            }
+        }
+    }
+}
